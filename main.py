f37577fc46538bf21a982c1a777f0e9c1df492dd
egorbober <114130184+egorbober@users.noreply.github.com>
2022-09-22 12:03:50 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# # a = input("Имя:")
+# # b = input("Возвраст:")
+# # c = input("Время:")
+# # print(f"Меня зовут {a},мне {b} лет,Сейчас
+# # a =int(input("a:"))
+# # b =int(input("a:"))
+# # P = (a + b) * 2
+# # S = a * b
+# # print ("P=", P)
+# # print ("S=", S)
+#  a =input("a: ")
+#  b =input("b: ")
+#  c = a
+#  a = b
+#  b = c
+#  print("a:", a)
+#  print("b:", b)
+#  a = int(input("число 1:"))
+#  b = int(input("число 2:"))
+#  print ((a + b) / 2)
+# a = int(input("часы:"))
+# b = int(input("минуты:"))
+# c = int(input("секунды:"))
+# print((a * 3600) +
