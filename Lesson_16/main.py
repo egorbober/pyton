a5107eba1b22e605df7fe32bbd668666d740e942
movavi_school <mdiscool1@mail.ru>
2023-01-12 12:34:07 +0700

    12 января

---
+++ Lesson_16/main.py
@@ -0,0 +1,95 @@
+#Словари
+
+# d = {}
+# d = dict{}
+#
+# d = {"Ключ1": 1,
+#      10: "Два",
+#      True :"Ложь"}
+
+
+# #Функция
+# def hello_World():
+#    print ("hello_World")
+#    print ("hello_World")
+
+# hello_World()
+# hello_World()
+
+
+# def functia(imya):
+#    print(imya,"777")
+
+
+# name = input("Что ты такое?:")
+# functia(imya = name)
+
+
+# def slojenie (chislo1, chislo2):
+#    result = chislo1 + chislo2
+#    return  result
+
+# print(slojenie(0,0))
+# x = slojenie(5,3)
+
+
+# def more_5(number):
+#    if number > 5 :
+#       return True
+
+# if more_5(8):
+#    print("Балдёжжжжж")
+
+
+# первый задача
+# def is_sorted(spisok):
+#    s = sorted(spisok)
+#    if spisok == s:
+#       return True
+
+# [1, 2, 5, 79, 128]
+# if is_sorted(spisok):
+#    print(f"Балдёж")
+# else:
+#    print("Я хочу пелмешки")
+
+
+# Второй задача
+# def find_longest(tadjiki : list()):
+#    francuzi = []
+
+#    for Rossiane in tadjiki:
+#       francuzi.append(len(Rossiane))
+#    maxim = max(francuzi)
+#    portugalki = francuzi.index(maxim)
+#    return tadjiki[portugalki],maxim
+
+# Uzbeki["Россия", "любит", "пельмени"]
+# print(find_longest(Uzbeki))
+
+
+# третий задача
+def max_min(spisok):
+     # shweizaria = min(spisok)
+     # scotlandia = max(spisok)
+
+     #    belorusi = sorted(spisok)
+     #    italianki = belorusi[0]
+     #    tailandki = belorusi[-1]
+     #    return italianki,tailianki
+     # spisok = [ 23,45,78,60,98]
+     # max_min(spisok)
+
+     # четвёртый задача
+     def is_prine(celoe_chislo):
+          for vietnamzi in range(2, celoe_chislo + 1):
+               if vietnamzi == celoe_chislo:
+                    pass
+               if celoe_chislo % vietnamzi == 0:
+                    break
+
+
+if is_prime(8758986):
+     print("ono prostoe")
+else:
+     print("ono natural")
